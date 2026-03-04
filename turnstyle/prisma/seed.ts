9fa64ae8540277684b4394a3dc948c38bd57b41b
Chris Scott <chrisscott@Mac.net>
2026-03-04 12:10:51 +1000

    fix: stringify address objects in seed

--- turnstyle/prisma/seed.ts
+++ turnstyle/prisma/seed.ts
@@ -10,17 +10,17 @@ async function main() {
       {
         name: 'Repco Parts Pty Ltd',
         abn: '97 097 993 283',
-        address: { street: '22 Enterprise Drive', city: 'Rowville', state: 'VIC', postcode: '3175' },
+        address: JSON.stringify({ street: '22 Enterprise Drive', city: 'Rowville', state: 'VIC', postcode: '3175' }),
       },
       {
         name: 'Rheem Australia Pty Limited',
         abn: '21 098 823 511',
-        address: { street: '1 Alan Street', city: 'Rydalmere', state: 'NSW', postcode: '2116' },
+        address: JSON.stringify({ street: '1 Alan Street', city: 'Rydalmere', state: 'NSW', postcode: '2116' }),
       },
       {
         name: 'Samsung Electronics Australia Pty Limited',
         abn: '63 002 915 648',
-        address: { street: '3 Murray Rose Avenue', city: 'Sydney Olympic Park', state: 'NSW', postcode: '2127' },
+        address: JSON.stringify({ street: '3 Murray Rose Avenue', city: 'Sydney Olympic Park', state: 'NSW', postcode: '2127' }),
       },
     ],
     skipDuplicates: true,
